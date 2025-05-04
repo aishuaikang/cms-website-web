755f14551a491bb14c8700e02f0427b80ce8cdbe
ask <2506377990@qq.com>
2025-05-04 19:35:40 +0800

    fix: 修复

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-  apps: [
-    {
-      name: 'cms-website-web',
-      script: 'bun',
-      args: 'start',
-      instances: 'max',  // 使用机器的所有CPU内核
-      exec_mode: 'cluster',  // Cluster模式
-      env: {
-        NODE_ENV: 'production'
-      }
-    }
-  ]
+    apps: [
+        {
+            name: "cms-website-web",
+            script: "bun",
+            args: "start",
+            instances: "max", // 使用机器的所有CPU内核
+            exec_mode: "cluster", // Cluster模式
+            env: {
+                NODE_ENV: "production",
+            },
+        },
+    ],
 };
-​
